6b877e8e47b0c28cb0a9c3ba2e1992444f9bb848
paaaaattt <145470405+paaaaattt@users.noreply.github.com>
2023-09-26 08:50:51 -0600

    Add files via upload

---
+++ ventanaprog/src/main/java/com/mycompany/ventanaprog/Formulario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pfr;
+import javax.swing.*;
+
+/**
+ *
+ * @author yo
+ */
+public class Formulario extends JFrame{
+    public Formulario(){
+        setLayout(null);
+    }
+    public static void main(String[] args) {
+        Formulario formulario1=new Formulario();
+        formulario1.setBounds(10, 20, 400, 300);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
+   
